f32ba84cd3f10aeb617e6b779c663428bea3a4b1
Anshul <Anshul@anshul.com>
2022-11-17 19:58:04 +0530

    initial commit

---
+++ encryption.js
@@ -0,0 +1,28 @@
+// aes algo to encrypt a string
+
+var crypto = require('crypto');
+var algorithm = 'aes-256-ctr';
+var password
+var iv = crypto.randomBytes(16);
+
+function encrypt(text){
+    var cipher = crypto.createCipheriv(algorithm,password,iv);
+    var crypted =
+    cipher
+    .update(text,'utf8','hex')
+    cipher.final('hex');
+    return crypted;
+}
+
+function decrypt(text){
+    var decipher = crypto.createDecipheriv(algorithm,password,iv);
+    var dec = decipher.update(text,'hex','utf8');
+    dec += decipher.final('utf8');
+    return dec;
+}
+
+var hw = encrypt("hello world")
+// outputs hello world
+
+console.log(decrypt(hw));
+// outputs hello world
