38e1e050f6ee6d8479e2565b7e54c43073d2827d
MohdOwais22 <mohdowais762@gmail.com>
2022-11-19 21:42:12 +0530

    fixed

--- index.js
+++ index.js
@@ -5,46 +5,45 @@ const bodyParser = require("body-parser");
 var crypto = require("crypto");
 const { type } = require("os");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.render("index");
+  res.render("index");
 });
 
 app.get("/encrypt", (req, res) => {
-// get the text from the form
-    var text = req.query.text;
-    var password = req.query.password;
-    
-    // encrypt the password 
-    const key = crypto.scryptSync(password, 'salt', 32);
-    var iv = crypto.randomBytes(16);
-    var cipher = crypto.createCipheriv("aes-256-ctr", key, iv);
-    var crypted = cipher.update(text, "utf8", "hex") + cipher.final("hex");
+  // get the text from the form
+  var text = req.query.text;
+  var password = req.query.password;
 
-    // render the encrypted text and decrypted text
-    res.render("encrypt", {
-        text: text,
-        password: password,
-        crypted_text: crypted,
-        // decrypted_text: decrypted
-    });
+  // encrypt the password
+  const key = crypto.scryptSync(password, "salt", 32);
+  var iv = crypto.randomBytes(16);
+  var cipher = crypto.createCipheriv("aes-256-ctr", key, iv);
+  var crypted = cipher.update(text, "utf8", "hex") + cipher.final("hex");
 
+  // render the encrypted text and decrypted text
+  res.render("encrypt", {
+    text: text,
+    password: password,
+    crypted_text: crypted,
+    // decrypted_text: decrypted
+  });
 
-    console.log(crypted);
+  console.log(crypted);
 });
 
 app.get("/decrypt", (req, res) => {
   var algorithm = "aes-256-ctr";
-  var password;
-  var iv = crypto.randomBytes(16);
-  var decipher = crypto.createDecipheriv(algorithm, password, iv);
-  var dec = decipher.update("hello world", "hex", "utf8");
-  dec += decipher.final("utf8");
-  res.send(dec);
+  let iv = Buffer.from(text.iv, "hex");
+  let encryptedText = Buffer.from(text.crypted, "hex");
+  let decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(key), iv);
+  let decrypted = decipher.update(encryptedText);
+  decrypted = Buffer.concat([decrypted, decipher.final()]);
+  return decrypted.toString();
 });
 
 app.listen(port, () => {
