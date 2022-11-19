51fbb90286725ec76c6d7883f726b9e14d09d290
MohdOwais22 <mohdowais762@gmail.com>
2022-11-19 22:31:03 +0530

    temp

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 var crypto = require("crypto");
-const { type } = require("os");
+const encryption = require("./encryption.js");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
@@ -14,36 +14,65 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
+// app.get("/encrypt", (req, res) => {
+//   // get the text from the form
+//   var text = req.query.text;
+//   var password = req.query.password;
+
+//   // encrypt the password
+//   const key = crypto.scryptSync(password, "salt", 32);
+//   var iv = crypto.randomBytes(16);
+//   var cipher = crypto.createCipheriv("aes-256-ctr", key, iv);
+//   var crypted = cipher.update(text, "utf8", "hex") + cipher.final("hex");
+//   // render the encrypted text and decrypted text
+//   res.render("encrypt", {
+//     text: text,
+//     password: password,
+//     crypted_text: crypted,
+//     // decrypted_text: decrypted
+//   });
+
+//   console.log(crypted);
+// });
+
 app.get("/encrypt", (req, res) => {
+  // import the encrypt function from encryption.js
+  const encrypt = encryption.encrypt;
   // get the text from the form
-  var text = req.query.text;
-  var password = req.query.password;
+  var text = req.body.text;
+  var password = req.body.password;
 
   // encrypt the password
-  const key = crypto.scryptSync(password, "salt", 32);
-  var iv = crypto.randomBytes(16);
-  var cipher = crypto.createCipheriv("aes-256-ctr", key, iv);
-  var crypted = cipher.update(text, "utf8", "hex") + cipher.final("hex");
-
+  var encrypted = encrypt(text, password);
   // render the encrypted text and decrypted text
   res.render("encrypt", {
     text: text,
     password: password,
-    crypted_text: crypted,
+    crypted_text: encrypted,
     // decrypted_text: decrypted
   });
+  console.log(encrypted);
 
-  console.log(crypted);
 });
 
-app.get("/decrypt", (req, res) => {
-  var algorithm = "aes-256-ctr";
-  let iv = Buffer.from(text.iv, "hex");
-  let encryptedText = Buffer.from(text.crypted, "hex");
-  let decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(key), iv);
+  
+
+function decrypt(text) {
+  let iv = Buffer.from(text.iv, 'hex');
+  let encryptedText = Buffer.from(text.encryptedData, 'hex');
+  let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
+ }
+
+
+app.get("/decrypt", (req, res) => {
+  decrypt(req.query.text);
+  res.render("decrypt", {
+    password: req.query.password,
+    decrypted_text: decrypt(req.query.text)
+  });
 });
 
 app.listen(port, () => {
