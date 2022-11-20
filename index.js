a9df3b58f04fb128999407a7f80bf24a65d41be2
Anshul <Anshul@anshul.com>
2022-11-20 10:54:44 +0530

    done with logic, only designing left

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const bodyParser = require("body-parser");
 var crypto = require("crypto");
 const md5 = require("md5");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
-
+// setting up the various modules
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
+// variables used
 const port = 3000;
 const algorithm = "aes-256-cbc";
 const key = crypto.randomBytes(32);
 const iv = crypto.randomBytes(16);
-
+const saltRounds = 10;
+// getting to the home route
 app.get("/", (req, res) => {
   res.render("index");
 });
-
+// algorithm for encrypting the password
 function encrypt(password, algoType) {
   switch (algoType) {
     case 1:
@@ -27,20 +27,45 @@ function encrypt(password, algoType) {
       let encrypted = cipher.update(password);
       encrypted = Buffer.concat([encrypted, cipher.final()]);
       return { iv: iv.toString("hex"), encryptedData: encrypted.toString("hex") };
-
     case 2:
       // Hashing algo
-      let pass = md5(password);
-      return pass;
-
+      let hash = md5(password);
+      return hash;
     case 3:
       // Salting algo
-      const hash = bcrypt.hashSync(password, saltRounds);
-      return hash;
-
+      const salt = bcrypt.hashSync(password, saltRounds);
+      return salt;
   }
 }
-
+app.get("/encrypt", (req, res) => {
+  // get the text from the form
+  var text = req.query.text;
+  var password = req.query.password;
+  var algoType = req.query.algo_chosen;
+  // passing the text and password to the encypt function
+  var crypted = encrypt(password, parseInt(algoType));
+  // render the encrypted text
+  if (algoType == 1) {
+    // for AES
+    res.render("encrypt", {
+      text: text,
+      password: password,
+      crypted_text: crypted,
+      algo_type: algoType,
+      crypted_text: crypted.encryptedData
+    });
+  } else {
+    // for hashing and salting
+    res.render("encrypt", {
+      text: text,
+      password: password,
+      crypted_text: crypted,
+      algo_type: algoType,
+      crypted_text: crypted
+    });
+  }
+});
+// function to decrypt the encrypted data
 function decrypt(text) {
   let iv = Buffer.from(text.iv, "hex");
   let encryptedText = Buffer.from(text.encryptedData, "hex");
@@ -48,29 +73,25 @@ function decrypt(text) {
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
-} 
-
-app.get("/encrypt", (req, res) => {
-  // get the text from the form
+}
+app.get("/decrypt", (req, res) => {
+  // getting the variables
   var text = req.query.text;
   var password = req.query.password;
-  var algoType = req.query.algo_chosen;
-
-  // passing the text and password to the encypt function
+  var algoType = req.query.algo_type;
+  // getting the objects through functions
   var crypted = encrypt(password, parseInt(algoType));
-  // var decrypted = decrypt(crypted);
-  
-  // render the encrypted text
-  res.render("encrypt", {
+  var decrypted_text = decrypt(crypted);
+  // rendering the decrypt.ejs file
+  res.render("decrypt", {
     text: text,
     password: password,
-    crypted_text: crypted, 
-    algo_type: algoType,
     crypted_text: crypted.encryptedData,
-    decrypted_text: decrypted
+    algo_type: algoType, 
+    decrypted_text: decrypted_text
   });
 });
-
+// listening to the port
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
